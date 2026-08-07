4f0e837f466a5ae515fccc4f171b5772c6c421f4
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2026-08-07 21:00:20 +0200

    Update proxy

--- WitGUI/src/app/app.config.ts
+++ WitGUI/src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     {
         provide: Configuration,
         useFactory: () => new Configuration({
-            basePath: '/api'
+            basePath: ''
         })
     }
   ]
