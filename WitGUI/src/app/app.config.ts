3d1c71bf7af49d044df5fee2ef87ee174671ed3a
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2026-08-07 20:02:11 +0200

    Update configuration gui

--- WitGUI/src/app/app.config.ts
+++ WitGUI/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { Configuration } from './api'
 
 import { routes } from './app.routes';
 
