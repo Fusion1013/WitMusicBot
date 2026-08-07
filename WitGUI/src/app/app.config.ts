ad1566dcfc3844a88c4dc3a5342cee64b7e07a15
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2026-08-07 20:33:34 +0200

    Remove config

--- WitGUI/src/app/app.config.ts
+++ WitGUI/src/app/app.config.ts
@@ -1,18 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { Configuration } from './api'
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
-    {
-        provide: Configuration,
-        useFactory: () => new Configuration({
-            basePath: '/app'
-        })
-    }
+    provideRouter(routes)
   ]
 };
