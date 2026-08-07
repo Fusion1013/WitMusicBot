355b1dce0daa08a7f79e222d9a20dc36b99dd85c
Fusion1013 <35802522+Fusion1013@users.noreply.github.com>
2026-08-07 20:35:25 +0200

    Update proxy config

--- WitGUI/src/app/app.config.ts
+++ WitGUI/src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { Configuration } from './api'
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+        provide: Configuration,
+        useFactory: () => new Configuration({
+            basePath: '/app'
+        })
+    }
   ]
 };
